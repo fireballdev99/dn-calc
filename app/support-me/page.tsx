bcdb24d8a2cb721de98064a6423ea19900180bab
fireballdev99 <fireball.dev99@gmail.com>
2025-05-19 13:51:16 +0700

    remove bmc widget

--- app/support-me/page.tsx
+++ app/support-me/page.tsx
@@ -6,27 +6,6 @@ import { Button } from '@heroui/react';
 import { HandThumbUpIcon } from '@heroicons/react/24/solid';
 
 export default function Page() {
-    // ✅ แทรก BuyMeCoffee Widget
-    useEffect(() => {
-        const script = document.createElement('script');
-        script.setAttribute('data-name', 'BMC-Widget');
-        script.setAttribute('data-cfasync', 'false');
-        script.src = 'https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js';
-        script.setAttribute('data-id', 'yodyxyz');
-        script.setAttribute('data-description', 'Support me on Buy me a coffee!');
-        script.setAttribute('data-message', 'Thank you for support me');
-        script.setAttribute('data-color', '#FF813F');
-        script.setAttribute('data-position', 'Right');
-        script.setAttribute('data-x_margin', '18');
-        script.setAttribute('data-y_margin', '18');
-        script.async = true;
-
-        document.body.appendChild(script);
-
-        return () => {
-            document.body.removeChild(script);
-        };
-    }, []);
 
     return (
         <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-orange-100 to-yellow-50">
